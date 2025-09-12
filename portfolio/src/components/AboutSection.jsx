0b08a70dd119f18fe1b7779884ed6db70826676e
iem-pranav <khairnar.pranav.nsk1@gmail.com>
2025-09-13 01:40:11 +0530

    Add resume

--- portfolio/src/components/AboutSection.jsx
+++ portfolio/src/components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="#contact"
+                href="Pranav-Resume.pdf" target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duartion-300"
               >
                 Download CV
