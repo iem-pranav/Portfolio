02fc2ce06fa0348b49c124bb0ea82ebf51bd7ddd
iem-pranav <khairnar.pranav.nsk1@gmail.com>
2025-09-12 23:15:59 +0530

    Update footer

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear()} <a href="https://github.com/iem-pranav">Pranav</a> All rights reserved.{" "}
+        &copy; {new Date().getFullYear()} <a href="https://github.com/iem-pranav">Pranav.</a> All rights reserved.{" "}
       </p>
       <a
         href="#hero"
